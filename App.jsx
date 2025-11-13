990e939859b719e586eb466bf238eba8af0b5bb5
Chauhan Jenil <chauhanjenilv05@gmail.com>
2025-11-13 11:24:07 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Board from './Board'
+import Todolist from './Todolist'
+import Lottery from './Lottery'
+import Form from './Form'
+
+function App() {
+
+  return (
+    <>
+      <Form />
+      <Todolist />
+      <Lottery />
+      {/* <Board /> */}
+    </>
+  )
+}
+
+export default App
